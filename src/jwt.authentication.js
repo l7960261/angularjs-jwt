1d245acdad1eb0d1e55010ab93310e90d46b7b93
l7960261 <l7960261@gmail.com>
2019-09-16 16:09:48 +0800

    Fix 'lodash' dependencies

--- src/jwt.authentication.js
+++ src/jwt.authentication.js
@@ -10,6 +10,7 @@ export default function jwtAuthenticationProvider() {
   this.changeOptions = (options) => {
     jwtOptions = assign(jwtOptions, options);
   };
+
   this.$get = ['$http', function ($http) {
     let accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3NhbXBsZXMuYXV0aDAuY29tLyIsInN1YiI6ImZhY2Vib29rfDEwMTU0Mjg3MDI3NTEwMzAyIiwiYXVkIjoiQlVJSlNXOXg2MHNJSEJ3OEtkOUVtQ2JqOGVESUZ4REMiLCJleHAiOjE0MTIyMzQ3MzAsImlhdCI6MTQxMjE5ODczMH0.7M5sAV50fF1-_h9qVbdSgqAnXVF7mz3I6RjS6JiH0H8';
 
